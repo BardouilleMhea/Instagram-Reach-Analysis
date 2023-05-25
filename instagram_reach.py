01d162116d2f4e25575944ad82e26a90cf837c6f
Mhea Bardouille <95055804+BardouilleMhea@users.noreply.github.com>
2023-05-24 22:53:48 -0400

    Update instagram_reach.py

--- instagram_reach.py
+++ instagram_reach.py
@@ -8,3 +8,7 @@ import plotly.express as px
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import PassiveAggressiveRegressor
+
+#read the dataset
+data = pd.read_csv("Instagram.csv", encoding = 'latin1')
+print(data.head())
